fe0e4d227d6021b0fc5c4dc389375f2277858430
somet3000 <karthikguruvayurappan@gmail.com>
2023-03-12 19:47:54 -0700

    added fine-mapping code

---
+++ src/features/create_fam118a_fine_mapping_low_coverage.R
@@ -0,0 +1,28 @@
+# read in RNA-seq matrix
+rnaseq.data <- read.table('data/out/low_coverage/matrix_eqtl_phenotype.txt')
+
+# isolate gene expression for FAM118A gene
+fam118a.id <- 'ENSG00000100376.7'
+fam118a.expr <- rnaseq.data[fam118a.id, ]
+
+# save gene expression values to a .txt file in susieR format
+fam118a.expr <- t(fam118a.expr)
+write.table(fam118a.expr, 'data/out/low_coverage/fam118a_expression.txt')
+
+# read in SNP info and gene info files
+snp.info <- read.table('data/out/low_coverage/snp_info.txt', header=TRUE)
+gene.info <- read.table('data/out/low_coverage/gene_info.txt', header=TRUE)
+
+# get coordinate for fam118a gene
+fam118a.coord <- gene.info[gene.info$geneid == fam118a.id, 'left']
+
+# get all candidate snps
+candidate.snps <- snp.info[abs(snp.info$pos - fam118a.coord) < 500000, ]
+
+# read in genotype matrix
+genotype.data <- read.table('data/out/low_coverage/matrix_eqtl_genotype_data.txt')
+
+# filter to only include genotypes for candidate snps
+genotype.data <- genotype.data[rownames(genotype.data) %in% candidate.snps$snpid, ]
+genotype.data <- t(genotype.data)
+write.table(genotype.data, 'data/out/low_coverage/fam118a_genotype.txt')
